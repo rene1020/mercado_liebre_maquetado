16943bdc1408f251ded02d1c39dc182fc88e644e
rene1020 <128187455+rene1020@users.noreply.github.com>
2023-10-12 11:19:19 -0300

    agrego la estrctura del home_html

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/music', (req, res) => res.sendFile(path.join(__dirname, 'views', 'music.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+
 
 app.listen(PORT,() => console.log(`Server Runing in http://localhost:${PORT}`));
